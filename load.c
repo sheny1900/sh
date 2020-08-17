32fd03c5f714f2cb5bab7054f2748a7a728d1474
sheny1900 <68209581+sheny1900@users.noreply.github.com>
2020-08-17 15:16:31 +0800

    Update load.c

--- load.c
+++ load.c
@@ -2,6 +2,22 @@
 cd /sys/kernel/debug/spmi/spmi-0 
 echo 0x844 > address && echo 1 > count && echo 0x00 > data && echo 0x845 > address && echo 0x00 > data && echo 0x846 > address && echo 0x01 > data && echo 0x847 > address && echo 0x80 > data 
 
+@进入模式：
+  edl（9008）
+    FORCED_USB_BOOT is connected to 1.8V ------硬件强制下载模式
+    "BOOT config" configure to boot from USB -----DP接地
+    3.PBL cannot run SBL normally 
+      3.1 no emmc 
+      3.2 cannot communicate with emmc 
+      3.3 emmc is empty 
+    4. secure boot pin is wrongly configured, or secure boot fuse is wrongly burned 
+    5 some error happened in SBL or LK, then it reboots to EDL; (In this situation, there is some log printed in UART) 
+    6 adb reboot edl
+  recovery:
+    adb reboot recovery
+    presistent apk 在一段时间内的频繁crash导致系统保护进入recovery模式
+    misc分区烧录boot-recovery的misc.img，recovery界面try again的misc.img
+   
 @电池欠压保护
 @Hysteresis 这个参数就是一个芯片恢复正常状态，也就是退出UVLO状态的电压差值，比如uvlo是2.7V ，Hysteresis 为100mv，那么电压升高到UVLO + Hysteresis 也就是2.8V，芯片就退出UVLO状态
 @low battery voltage ---> vph_pwr low ---> uvlo ---> warm reset (SDM450)
